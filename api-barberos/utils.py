66ddc58c173c57be2d5da7d64fbb636b85a50507
iNothingAtAll <inaa.from_abyss@proton.me>
2025-11-08 14:00:35 -0500

    [change] se implementaron las funciones útiles genéricas

--- api-barberos/utils.py
+++ api-barberos/utils.py
@@ -5,50 +5,62 @@ import requests
 
 
 
-def get_cliente_by_email(email):
+def get_model_by_attributes(model, attributes: dict):
     try:
-        cliente = Cliente.query.filter_by(correo=email).first()
+        element = model.query.filter_by(**attributes).first()
 
         return {
             'success': True,
-            'data': cliente.to_dict(),
+            'data': element.to_dict(),
         }
 
     except Exception as e:
         return {
             'success': False,
-            'error': str(e)
+            'error': f"Ocurrio un error al obtener el elemento de la tabla... {str(e)}"
         }
 
 
-def insertar_cliente(data):
-    nombre = data.get("nombre")
-    correo = data.get("correo")
-    telefono = data.get("telefono")
+def create_model_instance(model, data: dict):
+    try:
+        instance = model(**data)
+        db.session.add(instance)
+        db.session.commit()
 
-    cliente_nuevo = Cliente(
-        nombre=nombre, 
-        correo=correo, 
-        telefono=telefono
-    )
-
-    db.session.add(cliente_nuevo)
-    db.session.commit()
-    db.session.close()
-
-
-
-def create_token(data):
-    nombre = data.get("nombre")
-    correo = data.get("correo")
-    telefono = data.get("telefono")
-
-    access_token = create_access_token(
-        identity={
-            "nombre": nombre,
-            "correo": correo,
-            "telefono": telefono
+        return {
+            'success': True,
+            'data': instance.to_dict(),
         }
-    )
 
-    return access_token
+    except Exception as e:
+        db.session.rollback()
+        return {
+            'success': False,
+            'error': f"Ocurrio un error al crear el elemento de la tabla... {str(e)}"
+        }
+
+
+def create_client_token(data):
+    try:
+        nombre = data.get("nombre")
+        correo = data.get("correo")
+        telefono = data.get("telefono")
+
+        access_token = create_access_token(
+            identity={
+                "nombre": nombre,
+                "correo": correo,
+                "telefono": telefono
+            }
+        )
+
+        return {
+            'success': True,
+            'token': access_token
+        }
+    
+    except Exception as e:
+        return {
+            'success': False,
+            'error': f"Ocurrio un error al crear el token... {str(e)}"
+        }
